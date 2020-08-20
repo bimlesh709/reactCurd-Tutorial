4cfab68f8c1407518206a392b435c3bd53bee096
bimlesh <bimlesh10294@gmail.com>
2020-08-20 11:23:32 +0530

    initial changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
+import Edit from './component/Edit';
+import View from './component/View';
+import Insert from './component/Insert';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="container-fluid" style={{ alignSelf: 'stretch', }}>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <Link to={"/"} className="navbar-brand"><img src={logo} className="App-logo" alt="logo" /></Link>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item">
+                <Link to={"/"} className="nav-link">Home</Link>
+              </li>
+              <li className="nav-item">
+                <Link to={"/insert"} className="nav-link">Insert</Link>
+              </li>
+              <li className="nav-item">
+                <Link to={"/view"} className="nav-link">View</Link>
+              </li>
+              <li className="nav-item">
+                <Link to={"/"} className="nav-link">Test</Link>
+              </li>
+            </ul>
+
+          </div>
+        </nav>
+        <h2>Welcome</h2>
+        <Switch>
+          <Route exact path='/insert' component={Insert} />
+          <Route exact path='/edit/:id' component={Edit} />
+          <Route exact path='/view' component={View} />
+        </Switch>
+
+
+      </div>
+    </Router>
   );
 }
 
